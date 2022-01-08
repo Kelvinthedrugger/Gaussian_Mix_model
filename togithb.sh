3aef746d9287400b3ebde7d71915f794e564292f
kleesupernode0 <aangus0628@gmail.com>
2022-01-09 00:08:47 +0800

    commit it!

--- togithb.sh
+++
@@ -1,7 +0,0 @@
-echo "# Gaussian_Mix_model" # >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M master 
-git remote add origin https://github.com/Kelvinthedrugger/Gaussian_Mix_model.git
-git push -u origin master
