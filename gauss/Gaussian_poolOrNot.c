6e571ce32e22a908f3341c8ab42680eb8d3abe32
kleesupernode0 <aangus0628@gmail.com>
2022-01-11 16:48:53 +0800

    remove unused variables

--- gauss/Gaussian_poolOrNot.c
+++ gauss/Gaussian_poolOrNot.c
@@ -24,6 +24,13 @@ typedef struct{
 } Gauss_mixture_params;
 
 
+/*
+typedef struct{
+  double mu;
+  double sigma;
+} Gauss_params;
+*/
+
 // change here for different result ?
 const Gauss_params mu_prior_params= {0.0, 4.0};
 const double sigma_prior_param_a= 0.5;
@@ -32,11 +39,8 @@ const double sigma_prior_minval= 0.01;
 
 
 double data[DATA_N];
-const uint dataN= DATA_N;
-                 
-enum modelNames{ POOLED, DIFFER };
-                
-const uint sampleRepeatNum= 2000000;
+const int dataN= DATA_N;
+const int sampleRepeatNum= 2000000;
 
 
 int CMPup( const void *arg1, const void *arg2 ){
@@ -50,7 +54,7 @@ void println(){  printf( "\n" );  }
 
 double data_sample_mean(){
   double mean= 0.0;
-  for( uint i= 0;  i < dataN;  ++i ){
+  for( int i= 0;  i < dataN;  ++i ){
     mean += data[i];
   }
   return  mean / (double) dataN;
@@ -59,16 +63,18 @@ double data_sample_mean(){
 double data_sample_variance(){
   double mean= data_sample_mean();
   double var= 0.0;
-  for( uint i= 0;  i < dataN;  ++i ){
+  for( int i= 0;  i < dataN;  ++i ){
     double diff=  data[i] - mean;
     var +=  diff * diff;
   }
+  // frequencist ? 
   return  var / (double) (dataN-1);
 }
 
+
 void data_print(){
   qsort(  data,  dataN,  sizeof(double), CMPup  );
-  for( uint i= 0;  i < dataN;  ++i ){
+  for( int i= 0;  i < dataN;  ++i ){
     printf( "%d %+5.3f ", i, data[i] );
   }
 }
@@ -98,17 +104,17 @@ double cdfInv_JBeta[CDF_JBETA_N];  const double cdf_JBeta_n= CDF_JBETA_N;
 
 void cdfInv_precompute(){
   double x;
-  for(  uint i= 0; i < cdf_Gauss_n; ++i  ){
+  for(  int i= 0; i < cdf_Gauss_n; ++i  ){
     x= (i+1) / (double) (1+cdf_Gauss_n);
     cdfInv_Gauss[i]=  gsl_cdf_gaussian_Pinv( x, mu_prior_params.sigma );
     //printf( "cdfInv_Gauss[%u]= %g\n", i, cdfInv_Gauss[i] );
   }
-  for(  uint i= 0; i < cdf_gamma_n; ++i  ){
+  for(  int i= 0; i < cdf_gamma_n; ++i  ){
     x= i / (double) (cdf_gamma_n);
     cdfInv_gamma[i]=  sigma_prior_minval + gsl_cdf_gamma_Pinv( x, sigma_prior_param_a, sigma_prior_param_b );
     //printf( "cdfInv_Gamma[%u]= %g\n", i, cdfInv_gamma[i] );
   }
-  for(  uint i= 0; i < cdf_JBeta_n; ++i  ){
+  for(  int i= 0; i < cdf_JBeta_n; ++i  ){
     // tricks on symmetric, p <= 0.5
     x= 0.5 * i / (double) (cdf_JBeta_n);
     cdfInv_JBeta[i]=  gsl_cdf_beta_Pinv( x, 0.5, 0.5 );
@@ -117,13 +123,13 @@ void cdfInv_precompute(){
 }
 
 void data_generate_1component( Gauss_params params ){
-  for( uint i= 0; i < dataN; ++i ){
+  for( int i= 0; i < dataN; ++i ){
     data[i]=  GSLfun_ran_gaussian( params );
   }
 }
 
 void data_generate_2component( Gauss_mixture_params params ){
-  for( uint i= 0; i < dataN; ++i ){
+  for( int i= 0; i < dataN; ++i ){
     data[i]=  GSLfun_ran_gaussian
       (gsl_ran_flat01() < params.mixCof?  params.Gauss1  : params.Gauss2);
   }
@@ -133,13 +139,13 @@ void data_generate_2component( Gauss_mixture_params params ){
 
 double data_prob_1component_bySumming(){
   double prob_total= 0.0;
-  for(  uint m= 0;  m < cdf_Gauss_n;  ++m  ){
+  for(  int m= 0;  m < cdf_Gauss_n;  ++m  ){
     double mu= cdfInv_Gauss[m];
-    for(  uint s= 0;  s < cdf_gamma_n;  ++s  ){
+    for(  int s= 0;  s < cdf_gamma_n;  ++s  ){
       double sigma= cdfInv_gamma[s];
       Gauss_params cur_params= {mu, sigma};
       double curProb= 1.0;
-      for(  uint d= 0;  d < dataN;  ++d  ){
+      for(  int d= 0;  d < dataN;  ++d  ){
         double newProb= GSLfun_ran_gaussian_pdf( data[d], cur_params );
         curProb *= newProb;
       }
@@ -153,20 +159,20 @@ double data_prob_1component_bySumming(){
 double data_prob_2component_bySumming(){
   double prob_total= 0.0;
 
-  for(  uint m1= 0;  m1 < cdf_Gauss_n;  ++m1  ){
+  for(  int m1= 0;  m1 < cdf_Gauss_n;  ++m1  ){
     double mu1= cdfInv_Gauss[m1];
-    for(  uint m2= 0;  m2 < cdf_Gauss_n;  ++m2  ){
+    for(  int m2= 0;  m2 < cdf_Gauss_n;  ++m2  ){
       double mu2= cdfInv_Gauss[m2];
-      for(  uint s1= 0;  s1 < cdf_gamma_n;  ++s1  ){
+      for(  int s1= 0;  s1 < cdf_gamma_n;  ++s1  ){
         double sigma1= cdfInv_gamma[s1];
         Gauss_params cur_params1= {mu1, sigma1};
-        for(  uint s2= 0;  s2 < cdf_gamma_n;  ++s2  ){
+        for(  int s2= 0;  s2 < cdf_gamma_n;  ++s2  ){
           double sigma2= cdfInv_gamma[s2];
           Gauss_params cur_params2= {mu2, sigma2};
-          for(  uint mi= 0;  mi < cdf_JBeta_n;  ++mi  ){
+          for(  int mi= 0;  mi < cdf_JBeta_n;  ++mi  ){
             double mixCof= cdfInv_JBeta[mi];
             double curProb= 1.0;
-            for(  uint d= 0;  d < dataN;  ++d  ){
+            for(  int d= 0;  d < dataN;  ++d  ){
               double newProb=  mixCof  * GSLfun_ran_gaussian_pdf( data[d], cur_params1 )
                 +           (1-mixCof) * GSLfun_ran_gaussian_pdf( data[d], cur_params2 );
               curProb *= newProb;
@@ -186,10 +192,10 @@ double data_prob_2component_bySumming(){
 double data_prob_1component_bySampling(){
   double curProb, prob_total= 0.0;
 
-  for( uint iter= 0;  iter < sampleRepeatNum; ++iter ){
+  for( int iter= 0;  iter < sampleRepeatNum; ++iter ){
     Gauss_params params= prior_Gauss_params_sample();
     curProb= 1.0;
-    for(  uint d= 0;  d < dataN;  ++d ){
+    for(  int d= 0;  d < dataN;  ++d ){
       curProb *= GSLfun_ran_gaussian_pdf( data[d], params );
     }
     prob_total += curProb;
@@ -201,10 +207,10 @@ double data_prob_1component_bySampling(){
 double data_prob_2component_bySampling(){
   double curProb, prob_total= 0.0;
 
-  for( uint iter= 0;  iter < sampleRepeatNum; ++iter ){
+  for( int iter= 0;  iter < sampleRepeatNum; ++iter ){
     Gauss_mixture_params params=  prior_Gauss_mixture_params_sample();
     curProb= 1.0;
-    for( uint i= 0; i < dataN; ++i ){
+    for( int i= 0; i < dataN; ++i ){
       double newProb=
         (1-params.mixCof) * GSLfun_ran_gaussian_pdf( data[i], params.Gauss2 )
         +  params.mixCof  * GSLfun_ran_gaussian_pdf( data[i], params.Gauss1 );
@@ -225,15 +231,15 @@ int main( int argc, char *argv[] ){
   
   cdfInv_precompute();
 
-  const uint datasets_n= 10;
-  uint model1_sampling_favors1=  0;
-  uint model1_summing__favors1=  0;
-  uint model2_sampling_favors1=  0;
-  uint model2_summing__favors1=  0;
+  const int datasets_n= 10;
+  int model1_sampling_favors1=  0;
+  int model1_summing__favors1=  0;
+  int model2_sampling_favors1=  0;
+  int model2_summing__favors1=  0;
 
 
   printf( "\nData generated with one component\n" );
-  for(  uint iter= 0;  iter < datasets_n;  ++iter  ){
+  for(  int iter= 0;  iter < datasets_n;  ++iter  ){
     Gauss_params model_params = prior_Gauss_params_sample();
     printf(  "generating data with: (μ,σ) = (%4.2f,%4.2f)\n", model_params.mu, model_params.sigma  );
     data_generate_1component( model_params );
@@ -251,7 +257,7 @@ int main( int argc, char *argv[] ){
 
 
   printf( "\nData generated with two components\n" );
-  for(  uint iter= 0;  iter < datasets_n;  ++iter  ){
+  for(  int iter= 0;  iter < datasets_n;  ++iter  ){
     Gauss_mixture_params model_params=  prior_Gauss_mixture_params_sample();
     printf(  "generating data with:  m; (μ1,σ1); (μ2,σ2) = %5.3f; (%4.2f,%4.2f); (%4.2f,%4.2f)\n",
              model_params.mixCof,
